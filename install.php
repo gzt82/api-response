a6b8e866ed3fdc855060b3034515c88a0286f050
Ryan <11204356@qq.com>
2024-05-22 12:19:14 +0800

    自动安装

--- install.php
+++ install.php
@@ -1,14 +1,15 @@
 <?php
 $directories = [
     ['source' => __DIR__ . '/install/config', 'target' => __DIR__ . '/../../../config', 'description' => '配置文件部署成功'],
-    ['source' => __DIR__ . './install/library', 'target' => __DIR__ . '../../../app/Library', 'description' => '类库部署成功'],
+    ['source' => __DIR__ . './install/library', 'target' => __DIR__ . '/../../../app/Library', 'description' => '类库部署成功'],
 ];
 
 foreach ($directories as $directory) {
     $sourceDir = $directory['source'];
     $targetDir = $directory['target'];
 
-    if (!is_dir($targetDir) && !mkdir($targetDir, 0755, true) && !is_dir($targetDir)) {
+    // 创建目标目录，如果不存在
+    if (!file_exists($targetDir) && !mkdir($targetDir, 0755, true) && !is_dir($targetDir)) {
         throw new \RuntimeException(sprintf('Directory "%s" was not created', $targetDir));
     }
 
@@ -19,14 +20,17 @@ foreach ($directories as $directory) {
 
     foreach ($iterator as $item) {
         $targetPath = $targetDir . DIRECTORY_SEPARATOR . $iterator->getSubPathName();
-        if ($item->isDir()) {
-            if (!is_dir($targetPath) && !mkdir($targetPath, 0755, true) && !is_dir($targetPath)) {
+
+        // 如果是目录，尝试创建，如果失败抛出异常
+        if ($item->isDir() && !file_exists($targetPath)) {
+            if (!mkdir($targetPath, 0755, true) && !is_dir($targetPath)) {
                 throw new \RuntimeException(sprintf('Directory "%s" was not created', $targetPath));
             }
         } else {
+            // 如果是文件，直接复制
             copy($item, $targetPath);
         }
     }
 
-    echo $directory['description'];
+    echo $directory['description'] . "\n";
 }
