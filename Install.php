811805d504119b0460bd21bd05c0b7e7ea18c971
Ryan <11204356@qq.com>
2024-05-23 10:50:14 +0800

    自动安装

--- Install.php
+++ Install.php
@@ -1,46 +1,59 @@
 <?php
-$directories = [
-    ['source' => __DIR__ . '/install/config', 'target' => __DIR__ . '/../../../config', 'description' => '配置文件部署成功'],
-    ['source' => __DIR__ . './install/library', 'target' => __DIR__ . '/../../../app/Library', 'description' => '类库部署成功'],
-];
 
-foreach ($directories as $directory) {
-    $sourceDir = $directory['source'];
-    $targetDir = $directory['target'];
+namespace Ryantao\Scripts;
 
-    if (!file_exists($targetDir) && !mkdir($targetDir, 0755, true) && !is_dir($targetDir)) {
-        throw new \RuntimeException(sprintf('Directory "%s" was not created', $targetDir));
-    }
+use FilesystemIterator;
+use RecursiveDirectoryIterator;
+use RecursiveIteratorIterator;
 
-    $iterator = new RecursiveIteratorIterator(
-        new RecursiveDirectoryIterator($sourceDir, FilesystemIterator::SKIP_DOTS),
-        RecursiveIteratorIterator::SELF_FIRST
-    );
+class Install
+{
+    public static function postInstall(): void
+    {
+        $directories = [
+            ['source' => __DIR__ . '/install/config', 'target' => __DIR__ . '/../../../config', 'description' => '配置文件部署成功'],
+            ['source' => __DIR__ . './install/library', 'target' => __DIR__ . '/../../../app/Library', 'description' => '类库部署成功'],
+        ];
 
-    $allFilesExist = true;
-    foreach ($iterator as $item) {
-        if ($item->isFile()) {
-            $targetPath = $targetDir . DIRECTORY_SEPARATOR . $iterator->getSubPathName();
+        foreach ($directories as $directory) {
+            $sourceDir = $directory['source'];
+            $targetDir = $directory['target'];
 
-            echo "文件是否已安装：" . file_exists($targetPath);
-            if (!file_exists($targetPath)) {
-                $allFilesExist = false;
-                break;
+            if (!file_exists($targetDir) && !mkdir($targetDir, 0755, true) && !is_dir($targetDir)) {
+                throw new \RuntimeException(sprintf('Directory "%s" was not created', $targetDir));
             }
+
+            $iterator = new RecursiveIteratorIterator(
+                new RecursiveDirectoryIterator($sourceDir, FilesystemIterator::SKIP_DOTS),
+                RecursiveIteratorIterator::SELF_FIRST
+            );
+
+            $allFilesExist = true;
+            foreach ($iterator as $item) {
+                if ($item->isFile()) {
+                    $targetPath = $targetDir . DIRECTORY_SEPARATOR . $iterator->getSubPathName();
+
+                    if (!file_exists($targetPath)) {
+                        $allFilesExist = false;
+                        break;
+                    }
+                }
+            }
+
+            if ($allFilesExist) {
+                continue; // 跳过当前循环迭代
+            }
+
+            foreach ($iterator as $item) {
+                if ($item->isFile()) {
+                    $targetPath = $targetDir . DIRECTORY_SEPARATOR . $iterator->getSubPathName();
+                    copy($item, $targetPath);
+                }
+            }
+
+            echo $directory['description'] . "\n";
         }
     }
-
-    if ($allFilesExist) {
-        continue; // 跳过当前循环迭代
-    }
-
-    foreach ($iterator as $item) {
-        if ($item->isFile()) {
-            $targetPath = $targetDir . DIRECTORY_SEPARATOR . $iterator->getSubPathName();
-            copy($item, $targetPath);
-        }
-    }
-
-    echo $directory['description'] . "\n";
 }
 
+Install::postInstall();
