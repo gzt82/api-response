e1e819dd217f1a48f9011efbd12732c6bc349207
Ryan <11204356@qq.com>
2024-05-23 10:38:36 +0800

    自动安装

--- Install.php
+++ Install.php
@@ -1,60 +1,45 @@
 <?php
+$directories = [
+    ['source' => __DIR__ . '/install/config', 'target' => __DIR__ . '/../../../config', 'description' => '配置文件部署成功'],
+    ['source' => __DIR__ . './install/library', 'target' => __DIR__ . '/../../../app/Library', 'description' => '类库部署成功'],
+];
 
-namespace Ryantao\Scripts;
+foreach ($directories as $directory) {
+    $sourceDir = $directory['source'];
+    $targetDir = $directory['target'];
 
-use FilesystemIterator;
-use RecursiveDirectoryIterator;
-use RecursiveIteratorIterator;
+    if (!file_exists($targetDir) && !mkdir($targetDir, 0755, true) && !is_dir($targetDir)) {
+        throw new \RuntimeException(sprintf('Directory "%s" was not created', $targetDir));
+    }
 
-Install::postInstall();
+    $iterator = new RecursiveIteratorIterator(
+        new RecursiveDirectoryIterator($sourceDir, FilesystemIterator::SKIP_DOTS),
+        RecursiveIteratorIterator::SELF_FIRST
+    );
 
-class Install
-{
-    public static function postInstall(): void
-    {
-        $directories = [
-            ['source' => __DIR__ . '/install/config', 'target' => __DIR__ . '/../../../config', 'description' => '配置文件部署成功'],
-            ['source' => __DIR__ . './install/library', 'target' => __DIR__ . '/../../../app/Library', 'description' => '类库部署成功'],
-        ];
+    $allFilesExist = true;
+    foreach ($iterator as $item) {
+        if ($item->isFile()) {
+            $targetPath = $targetDir . DIRECTORY_SEPARATOR . $iterator->getSubPathName();
 
-        foreach ($directories as $directory) {
-            $sourceDir = $directory['source'];
-            $targetDir = $directory['target'];
-
-            if (!file_exists($targetDir) && !mkdir($targetDir, 0755, true) && !is_dir($targetDir)) {
-                throw new \RuntimeException(sprintf('Directory "%s" was not created', $targetDir));
+            if (!file_exists($targetPath)) {
+                $allFilesExist = false;
+                break;
             }
-
-            $iterator = new RecursiveIteratorIterator(
-                new RecursiveDirectoryIterator($sourceDir, FilesystemIterator::SKIP_DOTS),
-                RecursiveIteratorIterator::SELF_FIRST
-            );
-
-            $allFilesExist = true;
-            foreach ($iterator as $item) {
-                if ($item->isFile()) {
-                    $targetPath = $targetDir . DIRECTORY_SEPARATOR . $iterator->getSubPathName();
-
-                    if (!file_exists($targetPath)) {
-                        $allFilesExist = false;
-                        break;
-                    }
-                }
-            }
-
-            if ($allFilesExist) {
-                continue; // 跳过当前循环迭代
-            }
-
-            foreach ($iterator as $item) {
-                if ($item->isFile()) {
-                    $targetPath = $targetDir . DIRECTORY_SEPARATOR . $iterator->getSubPathName();
-                    copy($item, $targetPath);
-                }
-            }
-
-            echo $directory['description'] . "\n";
         }
     }
+
+    if ($allFilesExist) {
+        continue; // 跳过当前循环迭代
+    }
+
+    foreach ($iterator as $item) {
+        if ($item->isFile()) {
+            $targetPath = $targetDir . DIRECTORY_SEPARATOR . $iterator->getSubPathName();
+            copy($item, $targetPath);
+        }
+    }
+
+    echo $directory['description'] . "\n";
 }
 
