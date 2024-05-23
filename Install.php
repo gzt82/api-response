1753c7c5195dfae620ae89598330476c6a82ba6e
Ryan <11204356@qq.com>
2024-05-23 10:13:04 +0800

    自动安装

---
+++ Install.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace ryantao;
+
+use Composer\Script\Event;
+use RecursiveDirectoryIterator;
+use RecursiveIteratorIterator;
+use Symfony\Component\Filesystem\Filesystem;
+
+class Install
+{
+    public static function postInstall(Event $event)
+    {
+        $directories = [
+            ['source' => __DIR__ . '/install/config', 'target' => __DIR__ . '/../../../config', 'description' => '配置文件部署成功'],
+            ['source' => __DIR__ . './install/library', 'target' => __DIR__ . '/../../../app/Library', 'description' => '类库部署成功'],
+        ];
+
+        foreach ($directories as $directory) {
+            $sourceDir = $directory['source'];
+            $targetDir = $directory['target'];
+
+            if (!file_exists($targetDir) && !mkdir($targetDir, 0755, true) && !is_dir($targetDir)) {
+                throw new \RuntimeException(sprintf('Directory "%s" was not created', $targetDir));
+            }
+
+            $iterator = new RecursiveIteratorIterator(
+                new RecursiveDirectoryIterator($sourceDir, FilesystemIterator::SKIP_DOTS),
+                RecursiveIteratorIterator::SELF_FIRST
+            );
+
+            $allFilesExist = true;
+            foreach ($iterator as $item) {
+                if ($item->isFile()) {
+                    $targetPath = $targetDir . DIRECTORY_SEPARATOR . $iterator->getSubPathName();
+
+                    if (!file_exists($targetPath)) {
+                        $allFilesExist = false;
+                        break;
+                    }
+                }
+            }
+
+            if ($allFilesExist) {
+                continue; // 跳过当前循环迭代
+            }
+
+            foreach ($iterator as $item) {
+                if ($item->isFile()) {
+                    $targetPath = $targetDir . DIRECTORY_SEPARATOR . $iterator->getSubPathName();
+                    copy($item, $targetPath);
+                }
+            }
+
+            echo $directory['description'] . "\n";
+        }
+    }
+}
